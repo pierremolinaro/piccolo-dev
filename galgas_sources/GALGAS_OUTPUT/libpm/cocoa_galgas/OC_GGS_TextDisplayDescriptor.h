466b8ee803116ec7bd1c6d4603bc3cfe1c8216c1
Pierre Molinaro <pierre@pcmolinaro.name>
2011-12-09 16:15:47 +0000

    Mise à jour suite à MAJ GALGAS

--- galgas_sources/GALGAS_OUTPUT/libpm/cocoa_galgas/OC_GGS_TextDisplayDescriptor.h
+++ galgas_sources/GALGAS_OUTPUT/libpm/cocoa_galgas/OC_GGS_TextDisplayDescriptor.h
@@ -5,14 +5,19 @@
 //  Created by Pierre Molinaro on 24/11/11.
 //  Copyright (c) 2011 IRCCyN. All rights reserved.
 //
+//---------------------------------------------------------------------------*
 
 #import <Foundation/Foundation.h>
+
+//---------------------------------------------------------------------------*
 
 @class OC_GGS_TextSyntaxColoring ;
 @class OC_GGS_Document ;
 @class OC_GGS_TextView ;
 @class PMIssueDescriptor ;
 @class OC_GGS_RulerViewForTextView ;
+
+//---------------------------------------------------------------------------*
 
 @interface OC_GGS_TextDisplayDescriptor : NSObject <NSTextViewDelegate> {
 
@@ -39,6 +44,8 @@
 
 - (NSScrollView *) scrollView ;
 
+- (OC_GGS_Document *) document ;
+
 - (NSMenu *) menuForEntryPopUpButton ;
 
 - (NSUInteger) textSelectionStart ;
@@ -55,4 +62,8 @@
 - (void) setIssueArray: (NSArray *) inIssueArray ; 
 
 - (BOOL) makeVisibleIssue: (PMIssueDescriptor *) inIssue ;
+
+- (void) setSelectionRangeAndMakeItVisible: (NSRange) inRange ;
 @end
+
+//---------------------------------------------------------------------------*
