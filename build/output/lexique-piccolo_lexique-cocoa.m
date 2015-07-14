ee01638c39b89b5029104f1c3cba99be7e897faa
Pierre Molinaro <pierre@pcmolinaro.name>
2015-07-14 09:50:28 +0000

    Version 3.0.9

--- build/output/lexique-piccolo_lexique-cocoa.m
+++ build/output/lexique-piccolo_lexique-cocoa.m
@@ -3,6 +3,7 @@
 
 //--- END OF USER ZONE 1
 
+
 #import "lexique-piccolo_lexique-cocoa.h"
 #import "PMDebug.h"
 
@@ -914,5 +915,6 @@ static NSInteger search_into_piccolo_5F_lexique_instructionKeyWordList (NSString
 
 
 //--- END OF USER ZONE 2
+
 
 
